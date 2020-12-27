10f72e77ac9fdaba832825368c591c8f2a10dcb9
armanziaei <armanziaei83@gmaio.com>
2020-12-28 01:00:30 +0330

    Feat InfoDb.java Class : this class created for sqlite database basics

---
+++ app/src/main/java/com/example/sqlite/InfoDb.java
@@ -0,0 +1,24 @@
+package com.example.sqlite;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class InfoDb extends SQLiteOpenHelper {
+    public InfoDb(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, "info.db", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table student (name text , email text , age text)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("drop table if exists student");
+        onCreate(db);
+    }
+}
